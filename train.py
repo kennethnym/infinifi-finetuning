3f2b9e343d89d1af9225b97b984f0ede6af41092
Kenneth <kennethnym@outlook.com>
2024-08-23 21:37:42 +0100

    try to download single mp3 file from hf

--- train.py
+++ train.py
@@ -1,14 +1,16 @@
 import os
 import datasets
+from huggingface_hub import hf_hub_download
 
 from torch.utils.data import DataLoader
 
 os.environ["HF_HUB_ENABLE_HF_TRANSFER"] = "1"
 
 SAMPLE_RATE = 44100
+REPO = "vikhyatk/lofi"
 
 ds = datasets.load_dataset(
-    "vikhyatk/lofi",
+    REPO,
     use_auth_token=os.environ["TOKEN"],
     split="train",
     streaming=True,
@@ -16,5 +18,10 @@ ds = datasets.load_dataset(
 
 loader = DataLoader(ds, batch_size=32, num_workers=4)
 
-for row in iter(ds):
-    print(row)
+i = 0
+for row in iter(loader):
+    if i == 1:
+        break
+
+    relpath = row["audio"]["path"]
+    hf_hub_download(REPO, filename=relpath, local_dir="cache")
