eba6de3d73eb3f414b0172f8900d6df05f29010b
Kenneth <kenneth@nym.sh>
2026-07-27 15:58:56 +0100

    Use existing train permutation key (#20)

--- tests/test_train_script.py
+++ tests/test_train_script.py
@@ -82,7 +82,8 @@ class TrainScriptTest(unittest.TestCase):
         self.assertIn("checkpoint.save_every=5", arguments)
         self.assertIn("dataset.sample_on_weight=false", arguments)
         self.assertIn("dataset.sample_on_duration=false", arguments)
-        self.assertIn("+dataset.train.permutation_on_files=true", arguments)
+        self.assertIn("dataset.train.permutation_on_files=true", arguments)
+        self.assertNotIn("+dataset.train.permutation_on_files=true", arguments)
         self.assertNotIn("+dataset.permutation_on_files=true", arguments)
         self.assertFalse(
             any("word_dropout=" in argument for argument in arguments)
